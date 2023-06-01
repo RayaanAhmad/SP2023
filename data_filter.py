75597dfa48ef95111ac27c95b4f061021b8e21a7
Rayaan Ahmad <rahma4@illinois.edu>
2023-06-01 15:24:46 -0500

    added test print

--- data_filter.py
+++ data_filter.py
@@ -4,3 +4,5 @@ with open('BKB_WaterQualityData_2020084.csv') as BKB_Water:
     water_csv = csv.reader(BKB_Water, delimiter=',')
     with open('BKB_WaterCleaned.csv', mode='w') as BKB_Cleaned:
         cleaned_csv = csv.writer(BKB_Cleaned, delimiter=',')
+
+print("We're up to date!")
