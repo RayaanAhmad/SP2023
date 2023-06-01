2d3c66d556579952b2d38d599f94b81dec63526c
Rayaan Ahmad <rahma4@illinois.edu>
2023-06-01 15:15:36 -0500

    added filter.py

---
+++ data_filter.py
@@ -0,0 +1,6 @@
+import csv
+
+with open('BKB_WaterQualityData_2020084.csv') as BKB_Water:
+    water_csv = csv.reader(BKB_Water, delimiter=',')
+    with open('BKB_WaterCleaned.csv', mode='w') as BKB_Cleaned:
+        cleaned_csv = csv.writer(BKB_Cleaned, delimiter=',')
