e0c055cd53a377ac955be1ce8bdda4751f14020d
DariusVaz <dariusvazifdar@gmail.com>
2023-06-03 00:26:03 -0500

    Fixed bad formatting

--- data_filter.py
+++ data_filter.py
@@ -1,8 +1,8 @@
 import csv
 
-with open('BKB_WaterQualityData_2020084.csv') as BKB_Water:
-    water_csv = csv.reader(BKB_Water, delimiter=',')
-    with open('BKB_WaterCleaned.csv', mode='w') as BKB_Cleaned:
-        cleaned_csv = csv.writer(BKB_Cleaned, delimiter=',')
+with open('BKB_WaterQualityData_2020084.csv') as bkb_water:
+    water_csv = csv.reader(bkb_water, delimiter=',')
+    with open('BKB_WaterCleaned.csv', mode='w') as bkb_cleaned:
+        cleaned_csv = csv.writer(bkb_cleaned, delimiter=',')
 
 print("We're not up to date!")
