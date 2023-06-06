0604507e2de989161c3ce7636527a114027e196f
Rayaan Ahmad <rahma4@illinois.edu>
2023-06-06 00:44:12 -0500

    Added basic create graph func

--- analyze.py
+++ analyze.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-water_df = pd.read_csv("BKB_WaterCleaned.csv")
+water_df = pd.read_csv("BKB_WaterCleaned.csv") # Initialize the DataFrame on launch
 
 # Notes:
 # Site : [ A, B, C, D, Bay ]
@@ -24,6 +25,28 @@ def pick_two(site_df, param1, param2):
     paramed_df = site_df[ [param1, param2] ]
     return paramed_df
 
+
+# Function: takes given parameters and puts into a graph
+# @ Param: DataFrame: paramed_df - Dataframe with first column as X-Axis and second as Y-Axis
+# @ Returns: string: graph_name - name of the graph as .png file
+#            pyplot: image - the graph comparing the variables
+def create_graph(paramed_df):
+    name = "temp.png"
+    headers = paramed_df.columns # The headers in the dataframe
+
+    image = plt.figure() # The graph
+    plt.xlabel(headers[0]) # X-Axis = param1 from pick_two()
+    plt.ylabel(headers[1]) # Y-Axis = param2 from pick_two()
+
+    x_values = paramed_df[headers[0]]
+    y_values = paramed_df[headers[1]]
+    plt.plot(x_values, y_values) # Plots one vs the other
+
+    plt.title("Comparison")
+    plt.savefig(name)
+    return image, name
+
+# Arbritary Testing
 site = filter_site("A")
 params = pick_two(site, "Salinity (ppt)", "pH (standard units)")
-print(params)
+im, na = create_graph(params)
